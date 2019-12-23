fc4ff6be5ae13d0f2ee7a424e99c7728cfea16e5
greenbreakfast <lazardemin@gmail.com>
2019-12-23 11:19:06 -0500

    added script to be run on dash: downloads image file from given url and displays it on the tft

---
+++ scripts/image-dl-display.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# set the storage directory
+DIR="/tmp"
+
+# check for argument with image download url
+if [ "$1" == "" ]; then
+  echo "ERROR: expecting URL of image to display"
+  return 0
+fi
+
+# check image type 
+FILE=""
+jpg=$(echo $1 | grep -i jpg)
+png=$(echo $1 | grep -i png)
+if [ "$jpg" != "" ]; then
+  FILE=".jpg"
+elif [ "$png" != "" ]; then
+  FILE=".png"
+else 
+  echo "Expecting JPG or PNG image"
+fi
+
+
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
+FILENAME=$(echo "img_${NOW}${FILE}")
+
+# download the image file
+wget -O $FILENAME $1 
+
+# display to the screen
+fbi -d /dev/fb0 -notty -nocomments --noverbose $FILENAME
